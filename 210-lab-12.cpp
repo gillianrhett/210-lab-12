22f3ede4f23bb065937155d6dd95460d8079a502
gillianrhett <gillianrhett@gmail.com>
2026-02-22 09:56:40 -0800

    edited comments, var and const names

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -12,21 +12,23 @@ The requirements are:
 #include <fstream>
 #include <array>
 #include <string> // for the filename
+#include <algorithm> // for sort and find
+#include <numeric> // for accumilate
 using namespace std;
 
-const string filename = "infile.txt";
-const int numItems = 32;
+const string FILENAME = "infile.txt";
+const int SIZE = 32;
 
 int main() {
 /* Get items from a file that stores 1 integer per line,
 *  and store them in an array for the program to work with.
-*  This simulates a list of inventory counts we can initially
-*  get from a file then read, add, and remove items from the array.
+*  This simulates a situation where we keep track of the
+*  visitors a web page recieves each day.
 */
     ifstream inFile; // file object to get the items from
     // try to open the file
     try {
-        inFile.open(filename, ios::in);
+        inFile.open(FILENAME, ios::in);
         if(inFile.fail()) 
             throw invalid_argument("file not found");
     }
@@ -43,23 +45,20 @@ int main() {
         ++numItems;
     }  
     */
-    array <int, numItems> arr; // array to store items from file
+    array <int, SIZE> visitors; // array to store items from file
     // go back to the beginning so we can store the items in the array
     inFile.clear();
     inFile.seekg(ios::beg);
     int i = 0;
     // read each int from the file and store it in the array in the same order
     while(inFile >> tempInt){ // TODO add validation in case an item is not an int
-        arr.at(i) = tempInt;
+        visitors.at(i) = tempInt;
         ++i;
     }
     // now the ints from the file are in the array
 
-    // TESTING
-    for (int i = 0; i < numItems; ++i)
-        cout << arr.at(i) << endl;
-
     // TODO demonstration of array functions
+
 
     return 0;
 }
