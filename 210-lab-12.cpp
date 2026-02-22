720a6058aa951f022398b0584e1e59351ade6c24
gillianrhett <gillianrhett@gmail.com>
2026-02-21 18:35:11 -0800

    fixing where it reads ints from file

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -21,17 +21,33 @@ int main() {
     try {
         inFile.open(filename, ios::in);
         if(inFile.fail()) 
-            throw("file not found");
+            throw invalid_argument("file not found");
     }
     catch(invalid_argument& e) {
         cout << "Error: " << e.what();
         return -1;
     }
-    
-    int numItems = 0; // get the number of items the file contains
-    
+    int numItems = 0;
+    // loop through the file once to find out the number of items
+    int tempInt;
+    while(inFile >> tempInt){
+        ++numItems;
+    }  
 
     int* arr = new int[numItems]; // array to store items from file
+    inFile.seekg(ios::beg); // back to the beginning so we can store the items in the array
+    int i = 0;
+    while(inFile >> tempInt){
+        *(arr + i) = tempInt; // TODO fix this
+        ++i;
+    }
+    // now the ints from the file are in the array
+
+    // TESTING
+    for (int i = 0; i < numItems; ++i)
+        cout << *(arr + i) << endl;
+
+    // TODO demonstration of array functions
 
     return 0;
 }
