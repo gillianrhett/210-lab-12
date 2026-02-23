b3c71ae6ac9c7d4a8699c57d4d62a289b30374d8
gillianrhett <gillianrhett@gmail.com>
2026-02-22 16:48:22 -0800

    done

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -1,19 +1,11 @@
 // COMSC-210 | Lab 12 | Gillian Rhett
-/*Write a program that will exercise STD::array functions. 
-Code a real-world simulation of something that could be simulated by an array. 
-The requirements are:
-    X Use an STD::array
-    X Have an external data file with at least 30 elements in it
-    X Read the data into an STD::array 
-    _ Use a large variety of the <array> member functions from this module 
-        to demonstrate your understanding of the STD::array.*/
-
 #include <iostream>
 #include <fstream>
 #include <array>
-#include <string> // for the filename
+#include <string> // for the filename and input validation
 #include <algorithm> // for sort and find
-#include <numeric> // for accumilate
+#include <numeric> // for accumulate
+#include <cmath> // for checking whether file contents are ints
 using namespace std;
 
 const string FILENAME = "infile.txt";
@@ -22,7 +14,7 @@ const int SIZE = 32;
 int main() {
 /* Get items from a file that stores 1 integer per line,
 *  and store them in an array for the program to work with.
-*  This simulates a situation where we keep track of the
+*  This simulates a situation where we keep track of how many
 *  visitors a web page recieves each day.
 */
     ifstream inFile; // file object to get the items from
@@ -36,23 +28,27 @@ int main() {
         cout << "Error: " << e.what();
         return -1;
     }
-    int tempInt = 0;
-    /*
-    this part won't work with the array class, which needs a const
-    // loop through the file once to find out the number of items
-    int numItems = 0;
-    while(inFile >> tempInt){
-        ++numItems;
-    }  
-    */
+    
     array <int, SIZE> visitors; // array to store items from file
+    
     // go back to the beginning so we can store the items in the array
     inFile.clear();
     inFile.seekg(ios::beg);
     int i = 0;
     // read each int from the file and store it in the array in the same order
-    while(inFile >> tempInt){ // TODO add validation in case an item is not an int
-        visitors.at(i) = tempInt;
+    string tempInput; // for getting each line from the file
+    int tempInt; // for validating input
+    bool validInput = false;
+    while(i < SIZE && !inFile.eof()) {
+        inFile >> tempInput;
+        try {
+            tempInt = stoi(tempInput);
+            visitors.at(i) = tempInt;
+        }
+        catch(const exception& e) {
+            visitors.at(i) = 0; // if it's not an int, put 0 instead
+            inFile.clear();
+        }        
         ++i;
     }
     // now the ints from the file are in the array
@@ -108,5 +104,6 @@ int main() {
     sort(visitors.begin(), visitors.end() - SIZE / 2);
     for (int item : visitors) cout << item << " ";
     cout << endl;
+    cout << "16. accumulate function: " << accumulate(visitors.begin(), visitors.end(), 0);
     return 0;
 }
